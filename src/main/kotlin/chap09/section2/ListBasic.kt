4396ec2bc42180427449fac92b9e0d89788ad71c
박진우 <slogan138@gmail.com>
2022-08-22 20:48:43 +0900

    9장 수강(33%) 후 노트 작성

---
+++ src/main/kotlin/chap09/section2/ListBasic.kt
@@ -0,0 +1,17 @@
+package chap09.section2
+
+fun main() {
+    var numbers: List<Int> = listOf(1, 2, 3, 4, 5)
+    var names: List<String> = listOf("one", "two", "three")
+    var mixed = listOf("One", 1, 1.5, 'c')
+
+    println("numbers $numbers")
+    println("names $names")
+    println("mixed $mixed")
+
+    println(numbers.size)
+    println(numbers.indexOf(3))
+    println(numbers.get(0))
+    println(numbers[0])
+    println(numbers.contains(1))
+}
