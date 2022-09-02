9ca01750c842d6565c8ba78c7311505ba1e1b35a
박진우 <slogan138@gmail.com>
2022-09-02 18:01:43 +0900

    10장 수강(80%) 후 노트 작성

---
+++ src/main/kotlin/chap10/section3/FileWriteTest.kt
@@ -0,0 +1,18 @@
+package chap10.section3
+
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
+
+fun main() {
+    val path = "/Users/jinwoo/hello.txt"
+
+    val text = "Hello World, \n 안녕하세요\n"
+
+    try {
+        Files.write(Paths.get(path), text.toByteArray(), StandardOpenOption.CREATE)
+    } catch (exception: IOException) {
+        // Do Something
+    }
+}
